cf65c6e7c6b14ef2d71ae9083cf7feb88b1fef91
Durga Bhavani <durgabhavaniv77@gmail.com>
2026-05-21 12:11:25 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+# app.py
+
+def main():
+    print("---------------------------------")
+    print("Hello, Durga Bhavani!")
+    print("Welcome to your Git & Python practice!")
+    print("---------------------------------")
+
+if __name__ == "__main__":
+    main()
